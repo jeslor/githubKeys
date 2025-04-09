7c53f5ab7bc119ccf4cef1a7f01194fea984daad
Jeslor Ssozi <jbestius@gmail.com>
2025-04-09 15:43:37 +0200

    configure the process

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,19 @@ function App() {
   return (
     <>
       <h3>Hello there</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+        doloremque accusamus facere fugit ea cupiditate dolorem totam deleniti
+        blanditiis laudantium! Tenetur odio obcaecati mollitia placeat
+        temporibus, perferendis quisquam ipsum vel aliquid voluptas neque at
+        illum ducimus pariatur harum, inventore quia. Vitae autem et animi eos
+        eveniet magni nostrum dolorum, excepturi, repellendus vero aliquam nemo
+        quisquam. Reprehenderit fugit quasi nam laboriosam aspernatur nostrum
+        earum, reiciendis, maxime dicta inventore nihil blanditiis ea magni modi
+        necessitatibus ipsa laudantium amet vitae libero expedita quia harum ex.
+        Aut inventore non et, culpa earum nihil quibusdam. Itaque assumenda eos
+        consequatur vitae minus earum reprehenderit modi mollitia.
+      </p>
     </>
   );
 }
